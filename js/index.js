5089d63296f7658b807460180f4db710797a58b2
Nguyễn Hoàng Nam <congviec18120062@gmail.com>
2020-04-21 20:22:26 +0700

    🍉 Add photo _ Add blur photo in backgroun _ Add download button

--- js/index.js
+++ js/index.js
@@ -13,18 +13,22 @@ let titleCheck = title.getAttribute("data-choose") == "true";
 let contentCheck = content.getAttribute("data-choose") == "true";
 let sourceCheck = source.getAttribute("data-choose") == "true";
 
+let canvasBox = document.getElementById("canvas-box");
+let download = document.getElementById("download");
+
 let canvas = document.getElementById("viewport");
 let context = canvas.getContext("2d");
 
 let canvasWidth = canvas.width;
 let canvasHeight = canvas.height;
 
-context.fillStyle = "#ffffff";
+context.fillStyle = "#6f6f6f";
 context.fillRect(0, 0, canvasWidth, canvasHeight / 2);
 
 if (uploadImageCheck) {
 } else {
-  headerImage();
+  // headerImage();
+  demoImage();
 }
 
 context.moveTo(0, canvasHeight / 2);
@@ -55,6 +59,10 @@ if (sourceCheck) {
   showSource("Source");
 }
 
+// let canvasImageLink = canvas.toDataURL("image/png");
+let demoLink = "../img/demo.jpg";
+canvasBox.style.background = `url(${demoLink}) no-repeat top center`;
+
 //-----------------------------------------------
 
 function headerImage() {
@@ -62,6 +70,18 @@ function headerImage() {
   img.src = "../img/picture.svg";
   img.onload = function () {
     context.drawImage(img, canvasWidth / 2 - 25, canvasHeight / 4 - 10, 50, 50);
+  };
+}
+
+function demoImage() {
+  let img = new Image();
+  img.src = "../img/demo.jpg";
+  img.onload = function () {
+    let ratio = img.naturalWidth / img.naturalHeight;
+    let imageHeight = canvasHeight / 2;
+    let imageWidth = imageHeight * ratio;
+    let imageLeft = (canvasWidth - imageWidth) / 2;
+    context.drawImage(img, imageLeft, 0, imageWidth, imageHeight);
   };
 }
 
@@ -82,3 +102,20 @@ function showSource(source) {
   context.font = "italic 13px Verdana";
   context.fillText("Source", canvasWidth / 2, canvasHeight - 40);
 }
+
+function downloadCanvas(link, filename) {
+  // let canvasImage = new Image();
+  // canvasImage.src = canvas.toDataURL();
+  link.href = canvas.toDataURL();
+  link.download = filename;
+}
+
+//----------------------------------------------
+
+download.addEventListener(
+  "click",
+  function () {
+    downloadCanvas(this, "demo.png");
+  },
+  false
+);
